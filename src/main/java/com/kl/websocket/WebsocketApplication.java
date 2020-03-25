427bad169d5a5f9d0410fc9fdd94e227a6ab17f6
damai <damai@qunhemail.com>
2020-03-25 15:49:03 +0800

    1

--- src/main/java/com/kl/websocket/WebsocketApplication.java
+++ src/main/java/com/kl/websocket/WebsocketApplication.java
@@ -21,11 +21,11 @@ public class WebsocketApplication {
 
     int info = 1;
 
-    @Scheduled(fixedRate = 1000)
-    public void outputLogger() {
-        logger.info("测试日志输出" + info++);
-        throw new RuntimeException();
-    }
+//    @Scheduled(fixedRate = 1000)
+////    public void outputLogger() {
+////        logger.info("测试日志输出" + info++);
+////        throw new RuntimeException();
+////    }
 
     @RequestMapping("/hello")
     public String hello() {
